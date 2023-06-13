4488f17a5fd2c01e0e85bd769dc19ff7e697fcdf
岳栩彬 <yuexb@mail2.sysu.edu.cn>
2023-06-14 01:06:23 +0800

    修改了管理员的改，删，加

--- src/main/java/com/example/generator/service/IAdminService.java
+++ src/main/java/com/example/generator/service/IAdminService.java
@@ -18,4 +18,12 @@ public interface IAdminService extends IService<Admin> {
     Map<String, Object> login(Admin admin);
 
     Map<String, Object> getAdminInfo(String modifiedString);
+
+    boolean searchAdminByAccount(String account);
+
+    void changePasswordByAccount(String account, String password1);
+
+    void deleteAdminByAccount(String account);
+
+    void addAdmin(String account, String password1);
 }
